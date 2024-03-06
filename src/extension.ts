412f82fbfc02e45008ceb95eb5b5813d96046da5
Raphael BURRONI <raphael.burroni@mailspec.com>
2024-03-06 01:37:22 +0100

    fix: webview full size

--- src/extension.ts
+++ src/extension.ts
@@ -187,6 +187,18 @@ class TamagotchiGardenProvider implements vscode.WebviewViewProvider {
               <meta charset="UTF-8">
               <meta name="viewport" content="width=device-width, initial-scale=1.0">
               <title>Tamagotchi Garden</title>
+              <style>
+                body {
+                  padding: 0;
+                  margin: 0;
+                  width: 100%;
+                  overflow: hidden;
+                }
+                canvas {
+                  width: 100%;
+                  height: 100%;
+                }
+              </style>
             </head>
             <body>
               <script src="${scriptUriMain}"></script>
